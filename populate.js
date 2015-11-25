7edcff39956b86c9ece1d1d8d73d763a40d46a4f
nicowww <nicowww.montfort@gmail.com>
2015-11-25 17:25:38 +0100

    Update Posts Controller

--- populate.js
+++ populate.js
@@ -12,10 +12,13 @@ var current_post = 0;
 
 var debug = 1;
 
-//var json_output_file = "./.tmp/populate.json";
+// If you want to change the database name, you need to change
+// the sails config (config/connections.js)
+//var json_output_file = "./.tmp/localDiskDb.db";
 var json_output_file = "populate.json";
 var vdm_base_uri = "http://www.viedemerde.fr";
 
+// Delete database
 fs.unlink(json_output_file, function(err) {
    if (err) { return console.error(err); }
    console.log('File', json_output_file, 'deleted successfully!');
@@ -24,10 +27,12 @@ fs.unlink(json_output_file, function(err) {
 while (current_page < nb_pages) {
 	var vdm_uri = vdm_base_uri + "?page=" + current_page;
 	console.log('GET', vdm_uri, 'OK');
+	// Get vdm content
 	request({uri: vdm_uri}, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var $ = cheerio.load(body);
-			
+				
+				// Search datas
 				$('.article').each(function() {
 						var content = $(this).children().first().text();
 						var datas = $(this).children().next().children().next().children().next().text();
@@ -41,8 +46,10 @@ while (current_page < nb_pages) {
 							console.log(json_output);
 						}
 						
+						// Make a JSON
 						var json_output = JSON.stringify({content: content, date: line[1] + " " + line[3], author: line[8], id: nb_posts});
 						
+						// Write JSON into database
 						if (nb_posts > 0) {
 							fs.appendFileSync(json_output_file, json_output + ",");
 						}
